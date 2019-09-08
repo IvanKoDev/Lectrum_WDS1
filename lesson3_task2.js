02c753a2a57792f4df23958f58a49746c3d2435a
IvanKoDev <50876196+IvanKoDev@users.noreply.github.com>
2019-09-08 22:35:22 +0300

    Update lesson3_task2.js

--- lesson3_task2.js
+++ lesson3_task2.js
@@ -1,16 +1,21 @@
+// ЗАДАЧА 2
+// 1. Посчитайте количество элементов которе больше нуля.
+// 2. Посчитайте количество элементов которе меньше нуля.
+
+
 const numbers = [-91, 18, 11, -23, -98, 28, -81, -100, -7, -45, 52, 41, 75, -98, 27, 41, 25, 2, 39, -18];
    
-   let pozitiveCount = 0;
-   let negativeCount = 0;
+let pozitiveCount = 0;
+let negativeCount = 0;
    
-   for (let i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
        
-       if (numbers[i] > 0) {           // проверка на позитивность
-           pozitiveCount++;            // считаем кол-во позитивных элементов
-       } else if (numbers[i] < 0) {    // проверка на негативность
-           negativeCount++;            // считаем кол-во негативных элементов
-       }
+   if (numbers[i] > 0) {         // проверка на позитивность
+      pozitiveCount++;           // считаем кол-во позитивных элементов
+   } else if (numbers[i] < 0) {  // проверка на негативность
+      negativeCount++;           // считаем кол-во негативных элементов
    }
+}
    
-   console.log('pozitiveCount: ', pozitiveCount);  // pozitiveCount:  11
-   console.log('negativeCount: ', negativeCount);  // negativeCount:  9
+console.log('pozitiveCount: ', pozitiveCount);  // pozitiveCount:  11
+console.log('negativeCount: ', negativeCount);  // negativeCount:  9
