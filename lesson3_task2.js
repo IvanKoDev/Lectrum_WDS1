740199c5a16746e1f6b988787bf7f5359d2d477c
IvanKoDev <50876196+IvanKoDev@users.noreply.github.com>
2019-09-08 22:09:57 +0300

    Create lesson3_task2.js

---
+++ lesson3_task2.js
@@ -0,0 +1,16 @@
+const numbers = [-91, 18, 11, -23, -98, 28, -81, -100, -7, -45, 52, 41, 75, -98, 27, 41, 25, 2, 39, -18];
+   
+   let pozitiveCount = 0;
+   let negativeCount = 0;
+   
+   for (let i = 0; i < numbers.length; i++) {
+       
+       if (numbers[i] > 0) {           // проверка на позитивность
+           pozitiveCount++;            // считаем кол-во позитивных элементов
+       } else if (numbers[i] < 0) {    // проверка на негативность
+           negativeCount++;            // считаем кол-во негативных элементов
+       }
+   }
+   
+   console.log('pozitiveCount: ', pozitiveCount);  // pozitiveCount:  11
+   console.log('negativeCount: ', negativeCount);  // negativeCount:  9
